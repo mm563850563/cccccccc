88b72f7d45b549eadbabbe944e5552318ca19b76
lanou <lan@zhenghaiwendemac-mini.local>
2016-07-28 17:31:23 +0800

    DSA

---
+++ dsad/dsad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dsad
+//
+//  Created by lanou on 16/7/28.
+//  Copyright © 2016年 zhenghaiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dsad/dsad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dsad
+//
+//  Created by lanou on 16/7/28.
+//  Copyright © 2016年 zhenghaiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
